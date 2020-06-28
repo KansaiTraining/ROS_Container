69d2be58d1b741669446b407a0f6ac56858e907a
David Aliaga <adapt.robot.lab@gmail.com>
2020-06-28 22:13:25 +0900

    got things in shell scripts

--- build_run_kinetic.sh
+++ build_run_kinetic.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+
 docker build -t ros-kin-test .
 docker run --rm -it \
+       --name kin-test \
        -v "/home/pi/.ros/:/root/.ros/" \
        -v "/home/pi/first_ws/:/root/first_ws/" \
        ros-kin-test \
--- exec_run_kinetic.sh
+++ exec_run_kinetic.sh
@@ -1,2 +1,4 @@
-docker exec -it romantic_goldstine bash 
-source devel/setup.bash
+#!/bin/bash
+
+docker exec -it kin-test  /root/first_ws/sourceshell.sh
+#source devel/setup.bash
